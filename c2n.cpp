df3b87e1f98252674ee975b3b320adfc37ac7364
Michael <34414160+cbmuser@users.noreply.github.com>
2024-12-30 13:35:08 +0100

    Update c2n.cpp
    complete rewrite to class

--- c2n.cpp
+++ c2n.cpp
@@ -1,34 +1,32 @@
 #include <Arduino.h>
+#include <cstdint>
+#include "c2n.h"
 #include "hardware/timer.h"
 #include "hardware/irq.h"
-#include "c2n.h"
 
-int payload = 192;
-bool datasette;  
-bool isirq = true;
-char stream[192];
 
-void setup_ports() {
-  pinMode(writeport,OUTPUT);  
-  digitalWrite(writeport, LOW);
-  
-  pinMode(senseport,OUTPUT);  
-  digitalWrite(senseport, HIGH);
-  
-  pinMode(motorport,INPUT_PULLUP);    
 
-  pinMode(LED_BUILTIN,OUTPUT);  
-  digitalWrite(LED_BUILTIN, LOW);
-}
+void c2n::c2ninit() 
+  {
+    pinMode(writeport,OUTPUT);  
+    digitalWrite(writeport, LOW);
+    
+    pinMode(senseport,OUTPUT);  
+    digitalWrite(senseport, HIGH);
+    
+    pinMode(motorport,INPUT_PULLUP);    
+   
+    pinMode(readport,INPUT_PULLUP);    
+        
+    pinMode(LED_BUILTIN,OUTPUT);  
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 
-void delay_halfHz(uint32_t microseconds)
+void c2n::delay_Hz(uint32_t hz)
 {
      uint64_t ticks;
-     uint32_t base = 500000; 
-     
-     microseconds = base / microseconds;       
-
-         
+     uint32_t base = 500000;      
+     uint32_t microseconds = base / hz;       
      ticks = time_us_64();
 
 
@@ -43,114 +41,111 @@ void delay_halfHz(uint32_t microseconds)
 }
 
 
-bool no_irq() {
+bool c2n::no_irq() {
    noInterrupts();
     return isirq = false;
 }
 
-bool irq() {
+bool c2n::irq() {
    interrupts();
     return isirq = true;   
 }
 
 
-void write_pulse (float pulse ){
+void c2n::write_pulse (float pulse ){
     
     
     if (inverter) {
      digitalWrite(writeport, HIGH);
-     delay_halfHz(pulse);   
+     delay_Hz(pulse);   
      digitalWrite(writeport, LOW);
-     delay_halfHz(pulse);
+     delay_Hz(pulse);
       }
 
       else
 
       {
      digitalWrite(writeport, LOW);
-     delay_halfHz(pulse);     
+     delay_Hz(pulse);     
      digitalWrite(writeport, HIGH);
-     delay_halfHz(pulse);
+     delay_Hz(pulse);
       }
 }
 
 
+void c2n::set_long_pulse (){
 
-void set_long_pulse (){
-
- write_pulse (long_pulse);
+ c2n::write_pulse (long_pulse);
  
 }
 
-void set_medium_pulse (){
+void c2n::set_medium_pulse (){
 
- write_pulse (medium_pulse);
+ c2n::write_pulse (medium_pulse);
  
 }
 
-void set_short_pulse (){
+void c2n::set_short_pulse (){
 
- write_pulse (short_pulse);
+ c2n::write_pulse (short_pulse);
  
 }
 
-
-
-void send_bit(uint8_t value) {
+void c2n::send_bit(uint8_t value) {
   if (value) {
-    set_medium_pulse ();
-    set_short_pulse ();
+    c2n::set_medium_pulse ();
+    c2n::set_short_pulse ();
   } else {
-    set_short_pulse ();
-    set_medium_pulse ();
+    c2n::set_short_pulse ();
+    c2n::set_medium_pulse ();
   }
 }
 
 
-void send_byte(uint8_t value) {
+void c2n::send_byte(uint8_t value) {
   /* marker */
-    set_long_pulse ();
-    set_medium_pulse ();
+    c2n::set_long_pulse ();
+    c2n::set_medium_pulse ();
 
   bool parity = true;
   for (uint8_t i = 0; i < 8; i++) {
     uint8_t bit = value & 1;
 
-    send_bit(bit);
+    c2n::send_bit(bit);
     if (bit)
       parity = !parity;
 
     value >>= 1;
   }
 
-  send_bit(parity);
+  c2n::send_bit(parity);
 }
 
-void leader_intro(uint16_t length)
+
+
+void c2n::leader_intro(uint16_t length)
 
 {
 
    for (int i=0; i <= length;i++){
 
-     set_short_pulse ();
+     c2n::set_short_pulse ();
      
    }
 
   
 }
 
-void sync(uint8_t value) {
+void c2n::sync(uint8_t value) {
 
   for (uint8_t i = 9; i > 0; i--) {
   
-   send_byte(value + i);
+   c2n::send_byte(value + i);
   
   }
 }
 
-
-
-void loader (char* buffer) {
+void c2n::loader (char* buffer) {
 
  uint8_t pointer_lo= buffer[18];
  uint8_t pointer_hi= buffer[19];
@@ -201,7 +196,7 @@ Serial.println("LOADING ....");
 
 */
 
-digitalWrite(LED_BUILTIN, HIGH);
+ digitalWrite(LED_BUILTIN, HIGH);
    stream[0] = 0x01;
    stream[1] = pointer_lo;
    stream[2] = pointer_hi;
@@ -222,62 +217,89 @@ digitalWrite(LED_BUILTIN, HIGH);
  // Header
  //---------------------------------------
    digitalWrite(senseport, LOW);
-   no_irq();    
-    leader_intro(4000);
-    sync(0x80);
+    c2n::no_irq();    
+    c2n::leader_intro(4000);
+    c2n::sync(0x80);
 
   uint8_t checksum =0;
   for (uint8_t j = 0; j <= 191; j++) {
 
-    send_byte (stream[j]);
+    c2n::send_byte (stream[j]);
     checksum ^= stream[j];
   }
-     send_byte(checksum);
-     set_long_pulse();
+     c2n::send_byte(checksum);
+     c2n::set_long_pulse();
 
-     leader_intro(80);
-     sync(0x00);
+     c2n::leader_intro(80);
+     c2n::sync(0x00);
   checksum =0;
   for (uint8_t j = 0; j <= 191; j++) {
 
-    send_byte (stream[j]);
+    c2n::send_byte (stream[j]);
     checksum ^= stream[j];
   }
-     send_byte(checksum);
-     set_long_pulse ();
+     c2n::send_byte(checksum);
+     c2n::set_long_pulse ();
 
-     set_long_pulse ();
-     set_short_pulse ();
+     c2n::set_long_pulse ();
+     c2n::set_short_pulse ();
      digitalWrite(writeport, LOW);      
-     delay_halfHz(12);   
-     leader_intro(6000);
+     c2n::delay_Hz(12);   
+     c2n::leader_intro(6000);
     
 // FOUND ....
    
-    sync(0x80); 
+    c2n::sync(0x80); 
     checksum =0;
   for (uint16_t j = 20; j <= result+19; j++) {
 
-    send_byte (buffer[j]);
+    c2n::send_byte (buffer[j]);
     checksum ^= buffer[j];
     }  
-   send_byte(checksum);
-   set_long_pulse();
+   c2n::send_byte(checksum);
+   c2n::set_long_pulse();
 
-   leader_intro(80); 
-   sync(0x00);
+   c2n::leader_intro(80); 
+   c2n::sync(0x00);
    
   checksum =0;
   for (uint16_t j = 20; j <= result+19; j++) {
 
-    send_byte (buffer[j]);
+    c2n::send_byte (buffer[j]);
     checksum ^= buffer[j];
     }    
-    send_byte(checksum);
-    set_long_pulse ();
-    leader_intro(10);  
+    c2n::send_byte(checksum);
+    c2n::set_long_pulse ();
+    c2n::leader_intro(10);  
     digitalWrite(writeport, LOW);
-    irq();
+    c2n::irq();
     digitalWrite(senseport, HIGH);
     digitalWrite(LED_BUILTIN, LOW);
 }
+
+
+void c2n::toggle_sense(uint8_t _delay) {
+
+     delay(_delay);
+      digitalWrite(senseport, LOW);
+     delay(_delay);
+      digitalWrite(senseport, HIGH);
+     delay(_delay);
+ 
+}
+
+
+void c2n::set_sense (uint8_t value) {
+
+     digitalWrite(senseport, value);
+
+}
+
+bool c2n::motor(){
+
+   uint8_t is_motor = digitalRead(motorport); 
+   
+   return is_motor;
+   
+}
+
